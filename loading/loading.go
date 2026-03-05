9a65a0dcde9168bde9136190c1a596af1b80678e
rhuanpk <dev@rhuanpk.net>
2026-03-05 10:53:40 -0300

    feat: change to use channels

--- loading/loading.go
+++ loading/loading.go
@@ -1,10 +1,13 @@
 package loading
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 	"strings"
+	"sync/atomic"
+	"time"
 
 	"golang.org/x/term"
 )
@@ -15,22 +18,34 @@ const (
 )
 
 type bar struct {
-	stepsTotal int
-	stepsCount int
+	stepsTotal int64
+	stepsCount int64
 
-	termCols int
-	termRows int
+	termCols int64
+	termRows int64
+
+	quit  atomic.Bool
+	check chan int
+	done  chan struct{}
 }
 
-func NewBar(steps int) *bar {
+func NewBar(steps int64) *bar {
 	return &bar{
 		stepsTotal: steps,
 		stepsCount: 1,
+		check:      make(chan int, 1),
+		done:       make(chan struct{}, 1),
 	}
 }
 
-func (b bar) percentage() int {
-	return (b.stepsCount * 100) / b.stepsTotal
+func (b *bar) print() {
+	const layout = len("[] 000%")
+
+	cols := int(atomic.LoadInt64(&b.termCols))
+	chars := map[bool]int{true: cols - layout}[cols >= layout]
+	repeat := (chars * b.percentage()) / 100
+
+	fmt.Printf("[%s%s] %3d%%", strings.Repeat("#", repeat), strings.Repeat(".", chars-repeat), b.percentage())
 }
 
 func (b *bar) termSizeUpdate() {
@@ -40,42 +55,92 @@ func (b *bar) termSizeUpdate() {
 		log.Println("error geting terminal size:", err)
 		return
 	}
-	b.termCols = cols
-	b.termRows = rows
+
+	atomic.SwapInt64(&b.termCols, int64(cols))
+	atomic.SwapInt64(&b.termRows, int64(rows))
 }
 
-func (b bar) print() {
-	const layout = len("[] 000%")
+func (b *bar) percentage() int {
+	count := int(atomic.LoadInt64(&b.stepsCount))
+	total := int(atomic.LoadInt64(&b.stepsTotal))
+	percentage := (count * 100) / total
+	overflow := percentage > 100
 
-	chars := map[bool]int{true: b.termCols - layout}[b.termCols >= layout]
-	repeat := (chars * b.percentage()) / 100
+	if overflow {
+		b.quit.Store(true)
+		return 100
+	}
 
-	fmt.Printf("[%s%s] %3d%%", strings.Repeat("#", repeat), strings.Repeat(".", chars-repeat), b.percentage())
+	return percentage
 }
 
-func (b *bar) Render() {
+func (*bar) clear() {
+	time.Sleep(time.Millisecond * 500)
+	ansiClearNexts()
+}
+
+func (b *bar) draw() {
 	b.termSizeUpdate()
 
-	if b.stepsCount < b.stepsTotal {
-		ansiNewLine()
-		ansiCursorUp()
-	}
+	ansiNewLine()
+	ansiCursorUp()
 
 	ansiCursorSave()
 	ansiClearNexts()
-	ansiCursorEnd(b)
 
+	ansiCursorEnd(b)
 	b.print()
 
-	if b.stepsCount <= b.stepsTotal {
-		ansiCursorRestore()
-	}
+	ansiCursorRestore()
+}
 
-	if b.stepsCount >= b.stepsTotal {
-		ansiClearNexts()
-	}
+func (b *bar) Done() {
+	<-b.done
+}
 
-	b.stepsCount++
+func (b *bar) Step(count int) {
+	if !b.quit.Load() {
+		select {
+		case b.check <- count:
+		default:
+		}
+	}
+}
+
+func (b *bar) Render() context.CancelFunc {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		for {
+			if b.quit.Load() {
+				b.done <- struct{}{}
+				close(b.check)
+				close(b.done)
+				return
+			}
+			select {
+			case <-ctx.Done():
+				ansiCursorRestore()
+				ansiClearNexts()
+				b.quit.Store(true)
+			case count := <-b.check:
+				stepsCount := atomic.LoadInt64(&b.stepsCount)
+				stepsTotal := atomic.LoadInt64(&b.stepsTotal)
+				finished := stepsCount >= stepsTotal
+
+				b.draw()
+				if finished {
+					b.clear()
+					b.quit.Store(true)
+					break
+				}
+
+				atomic.AddInt64(&b.stepsCount, int64(count))
+			}
+		}
+	}()
+
+	return cancel
 }
 
 func ansiNewLine() {
@@ -87,7 +152,7 @@ func ansiCursorUp() {
 }
 
 func ansiCursorEnd(b *bar) {
-	fmt.Printf(ansiCSI+"%d;H", b.termRows) // move cursor to down
+	fmt.Printf(ansiCSI+"%d;H", atomic.LoadInt64(&b.termRows)) // move cursor to down
 }
 
 func ansiCursorSave() {
